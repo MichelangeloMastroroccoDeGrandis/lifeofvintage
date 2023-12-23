7f3da59eab1a575df2405f694ddcc1c75ca0b35e
MichelangeloMastroroccoDeGrandis <michelangelo.mdegrandis@agridecision.com>
2023-12-23 09:02:01 +0100

    contacts

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,8 +1,9 @@
+import styles from './css/Contacts.module.css';
 const Contacts = () => {
 return (
     <div id="contacts">
         <h1>Contacts</h1>
-        <div class="container">
+        <div className={styles.container}>
             <p>Email</p>
             <p>Sviluppato da: Michelangelo</p>
             <p>Lascia qui la tua email</p>
