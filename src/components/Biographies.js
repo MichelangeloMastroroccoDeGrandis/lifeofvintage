23951f0a7967b293ca4c6a6f470be61b0affc5be
MichelangeloMastroroccoDeGrandis <michelangelo.mdegrandis@agridecision.com>
2023-12-25 17:27:08 +0100

    some style

--- src/components/Biographies.js
+++ src/components/Biographies.js
@@ -1,7 +1,16 @@
 import styles from './css/Biographies.module.css';
+import arrow from './images/arrow.png';
+import check from './images/check-mark.png';
+
 const Biographies = ({arr, img}) => {
 
+    const thumbStyle = `
+        
+    `
+
+
     const textBio = arr.map(el => {
+        
         return  <div key={el.id} className={styles.bioWrap}>
                     <h2>{el.year}</h2>
                     <p>{el.description}</p>
@@ -10,10 +19,12 @@ const Biographies = ({arr, img}) => {
 
 return (
     <div id="biographies">
+        
         <h1>Biografie</h1>
         <div className={styles.container} >
             <div className={styles.backgroundImage} style={{backgroundImage: `url("${img}")`}}>
                 <div className={styles.textWrap}>
+                <style>{thumbStyle}</style>
                     {textBio}
                 </div>
             </div>
