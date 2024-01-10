d52586a4ce6897ee55ac4e4bbda51ae7a2597288
MichelangeloMastroroccoDeGrandis <michelangelo.mdegrandis@agridecision.com>
2024-01-10 07:51:55 +0100

    add on post online

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './components/css/App.css';
+import './css/App.css';
 import { motion } from 'framer-motion';
 import Header from './sections/Header';
 import Manifesto from './sections/Manifesto';
@@ -10,7 +10,7 @@ import amPic from './media/images/am.jpeg';
 import mdPic from './media/images/md.jpeg';
 import acPic from './media/images/ac.jpg';
 import am2Pic from './media/images/am2.jpg';
-import Socials from './components/Socials';
+
 import { useTranslation } from 'react-i18next'
 
 const App = () => {
@@ -30,15 +30,15 @@ const App = () => {
 
       <div className="App">
       <Header />
-      <Socials />
+
       <Manifesto />
       <Services />
       <div id="biographies">
         <motion.h1 initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} exit={{ opacity: 0}} transition={{duration: 1, delay: .5}}>Portraits</motion.h1>
         <Biographies arr={angeloCaroli} img={acPic} name="Angelo" surname="Caroli" link={ac_link} />
         <Biographies arr={maurizioDonadi} img={mdPic} name="Maurizio" surname="Donadi" link={md_link} />
-        <Biographies arr={antonioMastrorocco} img={amPic} name="Antonio" surname="Mastrorocco" link={am_link} />
         <Biographies arr={andreaMaffei} img={am2Pic} name="Andrea" surname="Maffei" link={am2_link} />
+        <Biographies arr={antonioMastrorocco} img={amPic} name="Antonio" surname="Mastrorocco" link={am_link} />
       </div>
       <Contacts />
     </div>
