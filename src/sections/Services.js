789fd6e03a418d34e718a1584e5667bec43a919e
MichelangeloMastroroccoDeGrandis <michelangelo.mdegrandis@agridecision.com>
2024-02-27 11:49:11 +0100

    all new tlov

--- src/sections/Services.js
+++ src/sections/Services.js
@@ -1,54 +1,53 @@
 import { motion } from "framer-motion";
 import styles from '../css/Services.module.css';
-import { useTranslation } from 'react-i18next'
+import { useTranslation } from 'react-i18next';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+import carousel1 from '../media/carousel/1.jpeg';
+import carousel2 from '../media/carousel/2.jpeg';
+import carousel3 from '../media/carousel/3.jpeg';
+import carousel4 from '../media/carousel/4.jpeg';
+import carousel5 from '../media/carousel/5.jpeg';
+import carousel6 from '../media/carousel/6.jpeg';
+import carousel7 from '../media/carousel/7.jpeg';
+
 const Services = ()=> {
     const { t } = useTranslation("global")
 return (
     <div id="services" className={styles.services}>
 
-        <motion.h1 initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} exit={{ opacity: 0}} transition={{duration: 1, delay: .5}}>What We Do</motion.h1>
+        <motion.h1 initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} exit={{ opacity: 0}} transition={{duration: 1, delay: .5}}>Projects</motion.h1>
 
         <div className={styles.container}>
             <motion.div id="aziende" initial={{ opacity: 0, y: 200 }} whileInView={{ opacity: 1, y: 0 }} transition={{duration: 2}} viewport={{ once: true }}>
 
-                <h2 className={styles.serviceListTitle}>{t("servizi.titleBrand")}</h2>
-                <ul className={styles.serviceList}>
-                    <li><p>{t("servizi.b1")}</p></li>
-                    <li><p>{t("servizi.b2")}</p></li>
-                    <li><p>{t("servizi.b3")}</p></li>
-                    <li><p>{t("servizi.b4")}</p></li>
-                    <li><p>{t("servizi.b5")}</p></li>
-                    <li><p>{t("servizi.b6")}</p></li>
-                    <li><p>{t("servizi.b7")}</p></li>
-                </ul>
-            </motion.div>
-            <motion.div id="retail" initial={{ opacity: 0, y: 200 }} whileInView={{ opacity: 1, y: 0 }} transition={{duration: 2}} viewport={{ once: true }}>
-           
-                <h2 className={styles.serviceListTitle}>{t("servizi.titleRetail")}</h2>
-                <ul className={styles.serviceList}>
-                    <li><p>{t("servizi.r1")}</p></li>
-                    <li><p>{t("servizi.r2")}</p></li>
-                    <li><p>{t("servizi.r3")}</p></li>
-                    <li><p>{t("servizi.r4")}</p></li>
-                    <li><p>{t("servizi.r5")}</p></li>
-                    <li><p>{t("servizi.r6")}</p></li>
-                    
-                </ul>
-            </motion.div>
-            <motion.div id="designer" initial={{ opacity: 0, y: 200 }} whileInView={{ opacity: 1, y: 0 }} transition={{duration: 2}} viewport={{ once: true }}>
-               
-                <h2 className={styles.serviceListTitle}>{t("servizi.titleDesigner")}</h2>
-                <ul className={styles.serviceList}>
-                    <li><p>{t("servizi.d1")}</p></li>
-                    <li><p>{t("servizi.d2")}</p></li>
-                    <li><p>{t("servizi.d3")}</p></li>
-                    <li><p>{t("servizi.d4")}</p></li>
-                    <li><p>{t("servizi.d5")}</p></li>
-                    <li><p>{t("servizi.d6")}</p></li>
-                    <li><p>{t("servizi.d7")}</p></li>
-                    <li><p>{t("servizi.d8")}</p></li>
+                    <p>{t("servizi.b1")}</p>
+
+                    <Carousel dynamicHeight="true">
+                <div className={styles.imageResize}>
+                    <img src={carousel1} alt="" />
+                </div>
+                <div className={styles.imageResize}>
+                    <img src={carousel2} alt="" />
+                </div>
+                <div className={styles.imageResize}>
+                    <img src={carousel3} alt="" />
+                </div>
+                <div className={styles.imageResize}>
+                    <img src={carousel4} alt="" />
+                </div>
+                <div className={styles.imageResize}>
+                    <img src={carousel5} alt="" />
+                </div>
+                <div className={styles.imageResize}>
+                    <img src={carousel6} alt="" />
+                </div>
+                <div className={styles.imageResize}>
+                    <img src={carousel7} alt="" />
+                </div>
                 
-                </ul>
+            </Carousel>
+
             </motion.div>
         </div>
     </div>
